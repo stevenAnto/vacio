120214754cc9ec6b2be94acf2161da433a71225b
STV <ecalcinap@unsa.edu.pe>
2022-04-28 17:22:46 -0500

    calculadora en proceso

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Calculator{
+
+  public int add(int a, int b){ 
+    return 0; 
+  }
+  public int sub(int a, int b){ 
+    return 0;
+  }
+  public int mul(int a, int b){ 
+    return 0; 
+  }
+  public int div(int a, int b){ 
+    return 0;
+  }
+  public int mod(int a, int b){  
+    return 0;
+  }
+}
